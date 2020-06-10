e0f8d41069671ce8d83f02cd015458f6e412a0bc
hadashiA <dev@hadashikick.jp>
2020-06-10 20:29:06 +0900

    Move to unity project

--- VContainer.Standalone/Properties/AssemblyInfo.cs
+++ VContainer.Standalone/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VContainer")]
+[assembly: AssemblyTitle("VContainer.Standalone")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VContainer")]
+[assembly: AssemblyProduct("VContainer.Standalone")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- VContainer/Assets/VContainer.Tests/ContainerTest.cs
+++ VContainer/Assets/VContainer.Tests/ContainerTest.cs
@@ -4,7 +4,6 @@ using NUnit.Framework;
 
 namespace VContainer.Tests
 {
-    [TestFixture]
     public class ContainerTest
     {
         [Test]
