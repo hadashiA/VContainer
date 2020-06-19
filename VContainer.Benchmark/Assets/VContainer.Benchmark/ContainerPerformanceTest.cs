437379fa50af4b611e3cdfb64befcab5e24df12f
hadashiA <dev@hadashikick.jp>
2020-06-19 14:23:49 +0900

    Fix performance tests

--- VContainer.Benchmark/Assets/VContainer.Benchmark/ContainerPerformanceTest.cs
+++ VContainer.Benchmark/Assets/VContainer.Benchmark/ContainerPerformanceTest.cs
@@ -2,15 +2,33 @@
 using Unity.PerformanceTesting;
 using VContainer;
 using VContainer.Benchmark.Fixtures;
+using Zenject;
 
 namespace Vcontainer.Benchmark
 {
-    public class VContainer
+    public class ContainerPerformanceTest
     {
         [Test]
         [Performance]
         public void ResolveSingleton()
         {
+            var zenjectContainer = new DiContainer();
+            zenjectContainer.Bind<ISingleton1>().To<Singleton1>().AsSingle();
+            zenjectContainer.Bind<ISingleton2>().To<Singleton2>().AsSingle();
+            zenjectContainer.Bind<ISingleton3>().To<Singleton3>().AsSingle();
+
+            Measure
+                .Method(() =>
+                {
+                    zenjectContainer.Resolve<ISingleton1>();
+                    zenjectContainer.Resolve<ISingleton2>();
+                    zenjectContainer.Resolve<ISingleton3>();
+                })
+                .SampleGroup("Zenject")
+                .WarmupCount(100)
+                .MeasurementCount(100)
+                .Run();
+
             var builder = new ContainerBuilder();
             builder.Register<ISingleton1, Singleton1>(Lifetime.Singleton);
             builder.Register<ISingleton2, Singleton2>(Lifetime.Singleton);
@@ -24,6 +42,7 @@ namespace Vcontainer.Benchmark
                     container.Resolve<ISingleton2>();
                     container.Resolve<ISingleton3>();
                 })
+                .SampleGroup("VContainer")
                 .WarmupCount(100)
                 .MeasurementCount(100)
                 .Run();
@@ -33,6 +52,35 @@ namespace Vcontainer.Benchmark
         [Performance]
         public void ResolveComplex()
         {
+            var zenjectContainer = new DiContainer();
+            zenjectContainer.Bind<IFirstService>().To<FirstService>().AsSingle();
+            zenjectContainer.Bind<ISecondService>().To<SecondService>().AsSingle();
+            zenjectContainer.Bind<IThirdService>().To<ThirdService>().AsSingle();
+            zenjectContainer.Bind<ISubObjectA>().To<SubObjectA>().AsTransient();
+            zenjectContainer.Bind<ISubObjectB>().To<SubObjectB>().AsTransient();
+            zenjectContainer.Bind<ISubObjectC>().To<SubObjectC>().AsTransient();
+            zenjectContainer.Bind<IComplex1>().To<Complex1>().AsTransient();
+            zenjectContainer.Bind<IComplex2>().To<Complex2>().AsTransient();
+            zenjectContainer.Bind<IComplex3>().To<Complex3>().AsTransient();
+            zenjectContainer.Bind<ISubObjectOne>().To<SubObjectOne>().AsTransient();
+            zenjectContainer.Bind<ISubObjectTwo>().To<SubObjectTwo>().AsTransient();
+            zenjectContainer.Bind<ISubObjectThree>().To<SubObjectThree>().AsTransient();
+
+            Measure
+                .Method(() =>
+                {
+                    UnityEngine.Profiling.Profiler.BeginSample("Zenject.ResolveComplex");
+                    zenjectContainer.Resolve<IComplex1>();
+                    zenjectContainer.Resolve<IComplex2>();
+                    zenjectContainer.Resolve<IComplex3>();
+                    UnityEngine.Profiling.Profiler.EndSample();
+                })
+                .SampleGroup("Zenject")
+                .WarmupCount(100)
+                .MeasurementCount(100)
+                // .GC()
+                .Run();
+
             var builder = new ContainerBuilder();
             builder.Register<IFirstService, FirstService>(Lifetime.Singleton);
             builder.Register<ISecondService, SecondService>(Lifetime.Singleton);
@@ -46,7 +94,6 @@ namespace Vcontainer.Benchmark
             builder.Register<ISubObjectOne, SubObjectOne>(Lifetime.Transient);
             builder.Register<ISubObjectTwo, SubObjectTwo>(Lifetime.Transient);
             builder.Register<ISubObjectThree, SubObjectThree>(Lifetime.Transient);
-
             var container = builder.Build();
 
             Measure
@@ -58,8 +105,10 @@ namespace Vcontainer.Benchmark
                     container.Resolve<IComplex3>();
                     UnityEngine.Profiling.Profiler.EndSample();
                 })
+                .SampleGroup("VContainer")
                 .WarmupCount(100)
                 .MeasurementCount(100)
+                // .GC()
                 .Run();
         }
     }
