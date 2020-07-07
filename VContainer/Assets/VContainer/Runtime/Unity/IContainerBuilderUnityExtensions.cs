1eacd621254e7ad92ea53cc7b12ddf6e5188b4b4
hadashiA <dev@hadashikick.jp>
2020-07-08 00:47:16 +0900

    Add builder.UseEntryPoints / .UseComponents

--- VContainer/Assets/VContainer/Runtime/Unity/IContainerBuilderUnityExtensions.cs
+++ VContainer/Assets/VContainer/Runtime/Unity/IContainerBuilderUnityExtensions.cs
@@ -1,9 +1,66 @@
+using System;
 using UnityEngine;
 
 namespace VContainer.Unity
 {
+    public readonly struct EntryPointsBuilder
+    {
+        readonly IContainerBuilder containerBuilder;
+        readonly Lifetime lifetime;
+
+        public EntryPointsBuilder(IContainerBuilder containerBuilder, Lifetime lifetime)
+        {
+            this.containerBuilder = containerBuilder;
+            this.lifetime = lifetime;
+        }
+
+        public RegistrationBuilder Add<T>()
+            => containerBuilder.Register<T>(lifetime).AsImplementedInterfaces();
+    }
+
+    public readonly struct ComponentsBuilder
+    {
+        readonly IContainerBuilder containerBuilder;
+
+        public ComponentsBuilder(IContainerBuilder containerBuilder)
+        {
+            this.containerBuilder = containerBuilder;
+        }
+
+        public RegistrationBuilder AddInstance(MonoBehaviour component)
+            => containerBuilder.RegisterComponent(component);
+
+        public RegistrationBuilder AddInHierarchy<T>() where T : MonoBehaviour
+            => containerBuilder.RegisterComponentInHierarchy<T>();
+
+        public ComponentRegistrationBuilder AddOnNewGameObject<T>(Lifetime lifetime, string newGameObjectName = null)
+            where T : MonoBehaviour
+            => containerBuilder.RegisterComponentOnNewGameObject<T>(lifetime, newGameObjectName);
+
+        public ComponentRegistrationBuilder AddInNewPrefab<T>(T prefab, Lifetime lifetime)
+            where T : MonoBehaviour
+            => containerBuilder.RegisterComponentInNewPrefab(prefab, lifetime);
+    }
+
     public static class IContainerBuilderUnityExtensions
     {
+        public static void UseEntryPoints(
+            this IContainerBuilder builder,
+            Lifetime lifetime,
+            Action<EntryPointsBuilder> configuration)
+        {
+            var entryPoints = new EntryPointsBuilder(builder, lifetime);
+            configuration(entryPoints);
+        }
+
+        public static void UseComponents(
+            this IContainerBuilder builder,
+            Action<ComponentsBuilder> configuration)
+        {
+            var components = new ComponentsBuilder(builder);
+            configuration(components);
+        }
+
         public static RegistrationBuilder RegisterEntryPoint<T>(
             this IContainerBuilder builder,
             Lifetime lifetime)
