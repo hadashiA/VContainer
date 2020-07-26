cb34428fff7c5b1763b14871afa59f8b410feeb3
hadashiA <dev@hadashikick.jp>
2020-07-26 23:09:05 +0900

    Fix lamdba expression allocation

--- VContainer/Assets/VContainer/Runtime/Internal/InjectorCache.cs
+++ VContainer/Assets/VContainer/Runtime/Internal/InjectorCache.cs
@@ -8,18 +8,16 @@ namespace VContainer.Internal
     {
         static readonly ConcurrentDictionary<Type, IInjector> Injectors = new ConcurrentDictionary<Type, IInjector>();
 
-        public static IInjector GetOrBuild(Type type)
-        {
-            return Injectors.GetOrAdd(type, type1 =>
-            {
-                var getter = type.GetMethod("__GetGeneratedInjector", BindingFlags.Static | BindingFlags.Public);
-                if (getter != null)
-                {
-                    return (IInjector)getter.Invoke(null, null);
-                }
+        public static IInjector GetOrBuild(Type type) => Injectors.GetOrAdd(type, Build);
 
-                return ReflectionInjector.Build(type);
-            });
+        static IInjector Build(Type type)
+        {
+            var getter = type.GetMethod("__GetGeneratedInjector", BindingFlags.Static | BindingFlags.Public);
+            if (getter != null)
+            {
+                return (IInjector)getter.Invoke(null, null);
+            }
+            return ReflectionInjector.Build(type);
         }
     }
 }
