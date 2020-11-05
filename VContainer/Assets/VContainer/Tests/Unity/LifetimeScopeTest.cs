0bbd9fecff0c55cc2583acc79b9c9a133aa3c466
hadashiA <dev@hadashikick.jp>
2020-11-06 00:03:56 +0900

    Add tests

--- VContainer/Assets/VContainer/Tests/Unity/LifetimeScopeTest.cs
+++ VContainer/Assets/VContainer/Tests/Unity/LifetimeScopeTest.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using UnityEngine;
 using NUnit.Framework;
+using UnityEngine.TestTools;
 using VContainer.Unity;
 
 namespace VContainer.Tests.Unity
@@ -19,6 +21,37 @@ namespace VContainer.Tests.Unity
 
             var child2 = new GameObject("LifetimeScope Child 2").AddComponent<LifetimeScope>();
             Assert.That(child2.Parent, Is.Null);
+        }
+
+        [UnityTest]
+        public IEnumerator ActivateEntryPoints()
+        {
+            using (LifetimeScope.Push(builder =>
+            {
+                builder.RegisterEntryPoint<SampleEntryPoint>(Lifetime.Scoped).AsSelf();
+            }))
+            {
+                var parentLifetimeScope = new GameObject("LifetimeScope").AddComponent<LifetimeScope>();
+                yield return null;
+
+                var parentEntryPoint = parentLifetimeScope.Container.Resolve<SampleEntryPoint>();
+                Assert.That(parentEntryPoint.InitializeCalled, Is.True);
+
+                var childLifetimeScope = parentLifetimeScope.CreateChild();
+                var childEntryPoint = childLifetimeScope.Container.Resolve<SampleEntryPoint>();
+                yield return null;
+
+                Assert.That(childEntryPoint.InitializeCalled, Is.True);
+                Assert.That(childEntryPoint, Is.Not.EqualTo(parentEntryPoint));
+
+                var scopeFactory = childLifetimeScope.Container.Resolve<IScopeFactory>();
+                var instantScope = scopeFactory.CreateScope();
+                yield return null;
+
+                var instantEntryPoint = instantScope.Resolve<SampleEntryPoint>();
+                Assert.That(instantEntryPoint.InitializeCalled, Is.True);
+                Assert.That(instantEntryPoint, Is.Not.EqualTo(childEntryPoint));
+            }
         }
     }
 }
