6adb6e1bfe4f01126fb04b73dd3b05bc15bbc3a1
hadashi <dev@hadashikick.jp>
2021-02-22 10:45:52 +0900

    Use naming convension *.CodeGen
    refs #138
    https://forum.unity.com/threads/how-does-unity-do-codegen-and-why-cant-i-do-it-myself.853867/#post-56469

--- VContainer/Assets/VContainer/Editor/CodeGen/VContainerILPostProcessor.cs
+++ VContainer/Assets/VContainer/Editor/CodeGen/VContainerILPostProcessor.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Unity.CompilationPipeline.Common.Diagnostics;
--- VContainer/Assets/VContainer/Runtime/AssemblyInfo.cs
+++ VContainer/Assets/VContainer/Runtime/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("VContainer.Tests")]
 [assembly: InternalsVisibleTo("VContainer.StandaloneTests")]
 [assembly: InternalsVisibleTo("VContainer.Editor")]
+[assembly: InternalsVisibleTo("Unity.VContainer.CodeGen")]
