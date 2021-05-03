c709466bd4c6f854c8b847c65c0c1c86272eb69d
hadashi <dev@hadashikick.jp>
2021-05-03 16:38:27 +0900

    1.8.0

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -64,7 +64,7 @@ module.exports = {
         },
         {
           href: 'https://github.com/hadashiA/VContainer/releases',
-          label: 'v1.7.2',
+          label: 'v1.8.0',
           position: 'right',
         },
         {
