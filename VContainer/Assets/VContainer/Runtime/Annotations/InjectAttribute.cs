ad03154940205901f07c3fb5c22814436184e8ec
Jesse Talavera-Greenberg <jesse@jesse.tg>
2021-08-12 18:19:28 -0400

    Document InjectAttribute

--- VContainer/Assets/VContainer/Runtime/Annotations/InjectAttribute.cs
+++ VContainer/Assets/VContainer/Runtime/Annotations/InjectAttribute.cs
@@ -6,6 +6,15 @@ namespace VContainer
     {
     }
 
+    /// <summary>
+    /// Any method, property, attribute, or constructor annotated with this attribute
+    /// will be used to inject dependencies.
+    /// </summary>
+    /// <remarks>
+    /// It is an error to apply this attribute to more than one constructor. However,
+    /// it can be applied to any number of methods, properties, or fields.
+    /// </remarks>
+    /// <seealso cref="IObjectResolver.Inject"/>
     [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
     public class InjectAttribute : PreserveAttribute
     {
