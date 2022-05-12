3ddbce491b597d7344850b8eace8702d26306ac9
hadashi <dev@hadashikick.jp>
2022-05-12 21:55:25 +0900

    Use JetBrains.Annotations implicit marker

--- VContainer/Assets/VContainer/Runtime/Annotations/InjectAttribute.cs
+++ VContainer/Assets/VContainer/Runtime/Annotations/InjectAttribute.cs
@@ -6,6 +6,9 @@ namespace VContainer
     {
     }
 
+#if UNITY_2018_4_OR_NEWER
+    [JetBrains.Annotations.MeansImplicitUse(JetBrains.Annotations.ImplicitUseKindFlags.InstantiatedNoFixedConstructorSignature)]
+#endif
     [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
     public class InjectAttribute : PreserveAttribute
     {
