02de05f7a9069238d6ff6b9067563351c8384901
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-07-29 11:29:55 -0700

    Fix InjectAttribute

--- VContainer/Assets/VContainer/Runtime/Annotations/InjectAttribute.cs
+++ VContainer/Assets/VContainer/Runtime/Annotations/InjectAttribute.cs
@@ -7,7 +7,7 @@ namespace VContainer
     }
 
 #if UNITY_2018_4_OR_NEWER
-    [JetBrains.Annotations.MeansImplicitUse(JetBrains.Annotations.ImplicitUseKindFlags.InstantiatedNoFixedConstructorSignature)]
+    [JetBrains.Annotations.MeansImplicitUse]
 #endif
     [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
     public class InjectAttribute : PreserveAttribute
