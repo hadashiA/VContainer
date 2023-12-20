eeb3b8aa1f9b41b52fe862ee0a5cbbd745b8e773
hadashiA <dev@hadashikick.jp>
2023-12-20 16:46:04 +0900

    Awaitable support

--- VContainer/Assets/VContainer/Runtime/Annotations/IAsyncStartable.cs
+++ VContainer/Assets/VContainer/Runtime/Annotations/IAsyncStartable.cs
@@ -9,4 +9,38 @@ namespace VContainer.Unity
         UniTask StartAsync(CancellationToken cancellation);
     }
 }
+#elif UNITY_2023_1_OR_NEWER
+using System;
+using System.Threading;
+using UnityEngine;
+
+namespace VContainer.Unity
+{
+    public interface IAsyncStartable
+    {
+        Awaitable StartAsync(CancellationToken cancellation);
+    }
+
+    static class AwaitableHelper
+    {
+        public static async Awaitable Forget(Awaitable awaitable, EntryPointExceptionHandler exceptionHandler)
+        {
+            try
+            {
+                await awaitable;
+            }
+            catch (Exception ex)
+            {
+                if (exceptionHandler != null)
+                {
+                    exceptionHandler.Publish(ex);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
 #endif
