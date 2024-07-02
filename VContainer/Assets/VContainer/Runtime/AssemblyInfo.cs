2e43a1f69531d10dc4ab19258cf6010e38ad1815
Pawel Krakowiak <pawel.krakowiak@rqgames.net>
2024-07-02 08:24:48 +0200

    Fix duplicate injection issue + introduce Editor tests

--- VContainer/Assets/VContainer/Runtime/AssemblyInfo.cs
+++ VContainer/Assets/VContainer/Runtime/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("VContainer.Tests")]
+[assembly: InternalsVisibleTo("VContainer.UnitTests")]
 [assembly: InternalsVisibleTo("VContainer.StandaloneTests")]
 [assembly: InternalsVisibleTo("VContainer.Editor")]
 [assembly: InternalsVisibleTo("Unity.VContainer.CodeGen")]
