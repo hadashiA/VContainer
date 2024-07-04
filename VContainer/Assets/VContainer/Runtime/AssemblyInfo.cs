418cc18b8d34cb88fac354ce0b50cf1e9bb07efb
Pawel Krakowiak <pawel.krakowiak@rqgames.net>
2024-07-04 09:04:19 +0200

    Move DuplicateInjection test to TypeAnalyzerTest

--- VContainer/Assets/VContainer/Runtime/AssemblyInfo.cs
+++ VContainer/Assets/VContainer/Runtime/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("VContainer.Tests")]
-[assembly: InternalsVisibleTo("VContainer.UnitTests")]
 [assembly: InternalsVisibleTo("VContainer.StandaloneTests")]
 [assembly: InternalsVisibleTo("VContainer.Editor")]
 [assembly: InternalsVisibleTo("Unity.VContainer.CodeGen")]
