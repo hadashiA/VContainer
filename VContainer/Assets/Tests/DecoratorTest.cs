b6861ac7c671c35da5be58d236ffbc8155b025bc
Valery Zhuk <zhuk.valeriy.98@gmail.com>
2025-07-17 23:33:52 +0300

    Fix decorator binding to all bound inner type interfaces

--- VContainer/Assets/Tests/DecoratorTest.cs
+++ VContainer/Assets/Tests/DecoratorTest.cs
@@ -3,34 +3,6 @@ using VContainer.Runtime;
 
 namespace VContainer.Tests
 {
-    interface IDecoratedType
-    {
-    }
-
-    class DecoratedType : IDecoratedType
-    {
-    }
-
-    class Decorator1 : IDecoratedType
-    {
-        public readonly IDecoratedType Inner;
-
-        public Decorator1(IDecoratedType inner)
-        {
-            Inner = inner;
-        }
-    }
-
-    class Decorator2 : IDecoratedType
-    {
-        public readonly IDecoratedType Inner;
-
-        public Decorator2(IDecoratedType inner)
-        {
-            Inner = inner;
-        }
-    }
-
     [TestFixture]
     public class DecoratorTest
     {
@@ -38,68 +10,189 @@ namespace VContainer.Tests
         public void Decorate()
         {
             var builder = new ContainerBuilder();
-            builder.Register<IDecoratedType, DecoratedType>(Lifetime.Singleton);
-            builder.RegisterDecorator<IDecoratedType, Decorator1>();
+            builder.Register<Mocks.IDecoratedType, Mocks.DecoratedType>(Lifetime.Singleton);
+            builder.RegisterDecorator<Mocks.IDecoratedType, Mocks.Decorator1>();
 
             var container = builder.Build();
-            var instance = container.Resolve<IDecoratedType>();
-            Assert.That(instance, Is.TypeOf<Decorator1>());
+            var instance = container.Resolve<Mocks.IDecoratedType>();
+            Assert.That(instance, Is.TypeOf<Mocks.Decorator1>());
             
-            var inner = ((Decorator1)instance).Inner;
-            Assert.That(inner, Is.TypeOf<DecoratedType>());
-        }
-
-        [Test]
-        public void Decorate_Func()
-        {
-            var builder = new ContainerBuilder();
-            builder.Register<IDecoratedType, DecoratedType>(Lifetime.Singleton);
-            builder.RegisterDecorator<IDecoratedType, Decorator1>((original, container) => new Decorator1(original));
-
-            var container = builder.Build();
-            var instance = container.Resolve<IDecoratedType>();
-            Assert.That(instance, Is.TypeOf<Decorator1>());
-            
-            var inner = ((Decorator1)instance).Inner;
-            Assert.That(inner, Is.TypeOf<DecoratedType>());
+            var inner = ((Mocks.Decorator1)instance).Inner;
+            Assert.That(inner, Is.TypeOf<Mocks.DecoratedType>());
         }
 
         [Test]
         public void Decorate_Nested()
         {
             var builder = new ContainerBuilder();
-            builder.Register<IDecoratedType, DecoratedType>(Lifetime.Singleton);
-            builder.RegisterDecorator<IDecoratedType, Decorator1>();
-            builder.RegisterDecorator<IDecoratedType, Decorator2>();
+            builder.Register<Mocks.IDecoratedType, Mocks.DecoratedType>(Lifetime.Singleton);
+            builder.RegisterDecorator<Mocks.IDecoratedType, Mocks.Decorator1>();
+            builder.RegisterDecorator<Mocks.IDecoratedType, Mocks.Decorator2>();
             
             var container = builder.Build();
-            var instance = container.Resolve<IDecoratedType>();
-            Assert.That(instance, Is.TypeOf<Decorator2>());
+            var instance = container.Resolve<Mocks.IDecoratedType>();
+            Assert.That(instance, Is.TypeOf<Mocks.Decorator2>());
             
-            var inner2 = ((Decorator2)instance).Inner;
-            Assert.That(inner2, Is.TypeOf<Decorator1>());
+            var inner2 = ((Mocks.Decorator2)instance).Inner;
+            Assert.That(inner2, Is.TypeOf<Mocks.Decorator1>());
             
-            var inner1 = ((Decorator1)inner2).Inner;
-            Assert.That(inner1, Is.TypeOf<DecoratedType>());
+            var inner1 = ((Mocks.Decorator1)inner2).Inner;
+            Assert.That(inner1, Is.TypeOf<Mocks.DecoratedType>());
+        }
+        
+        [Test]
+        public void Decorate_Func()
+        {
+            var builder = new ContainerBuilder();
+            builder.Register<Mocks.IDecoratedType, Mocks.DecoratedType>(Lifetime.Singleton);
+            builder.RegisterDecorator<Mocks.IDecoratedType, Mocks.Decorator1>(
+                (original, container) => new Mocks.Decorator1(original));
+
+            var container = builder.Build();
+            var instance = container.Resolve<Mocks.IDecoratedType>();
+            Assert.That(instance, Is.TypeOf<Mocks.Decorator1>());
+            
+            var inner = ((Mocks.Decorator1)instance).Inner;
+            Assert.That(inner, Is.TypeOf<Mocks.DecoratedType>());
         }
         
         [Test]
         public void Decorate_Func_Nested()
         {
             var builder = new ContainerBuilder();
-            builder.Register<IDecoratedType, DecoratedType>(Lifetime.Singleton);
-            builder.RegisterDecorator<IDecoratedType, Decorator1>((original, container) => new Decorator1(original));
-            builder.RegisterDecorator<IDecoratedType, Decorator2>((decorated, container) => new Decorator2(decorated));
+            builder.Register<Mocks.IDecoratedType, Mocks.DecoratedType>(Lifetime.Singleton);
+            builder.RegisterDecorator<Mocks.IDecoratedType, Mocks.Decorator1>(
+                (original, container) => new Mocks.Decorator1(original));
+            builder.RegisterDecorator<Mocks.IDecoratedType, Mocks.Decorator2>(
+                (decorated, container) => new Mocks.Decorator2(decorated));
             
             var container = builder.Build();
-            var instance = container.Resolve<IDecoratedType>();
-            Assert.That(instance, Is.TypeOf<Decorator2>());
+            var instance = container.Resolve<Mocks.IDecoratedType>();
+            Assert.That(instance, Is.TypeOf<Mocks.Decorator2>());
             
-            var inner2 = ((Decorator2)instance).Inner;
-            Assert.That(inner2, Is.TypeOf<Decorator1>());
+            var inner2 = ((Mocks.Decorator2)instance).Inner;
+            Assert.That(inner2, Is.TypeOf<Mocks.Decorator1>());
             
-            var inner1 = ((Decorator1)inner2).Inner;
-            Assert.That(inner1, Is.TypeOf<DecoratedType>());
+            var inner1 = ((Mocks.Decorator1)inner2).Inner;
+            Assert.That(inner1, Is.TypeOf<Mocks.DecoratedType>());
+        }
+
+        [Test]
+        public void Decorate_Does_Not_Bind_To_Non_Related_Interfaces_Of_Decorated_Type()
+        {
+            var builder = new ContainerBuilder();
+            builder.Register<Mocks.DecoratedTypeWithNonRelatedInterface>(Lifetime.Singleton)
+                .As<Mocks.IDecoratedType, Mocks.IOtherInterface>();
+            
+            builder.RegisterDecorator<Mocks.IDecoratedType, Mocks.Decorator1>();
+
+            var container = builder.Build();
+            var instance = container.Resolve<Mocks.IDecoratedType>();
+            Assert.That(instance, Is.TypeOf<Mocks.Decorator1>());
+            
+            var otherInstance = container.Resolve<Mocks.IOtherInterface>();
+            Assert.That(otherInstance, Is.TypeOf<Mocks.DecoratedTypeWithNonRelatedInterface>());
+        }
+
+        [Test]
+        public void Decorate_Supports_Binding_To_Additional_Interfaces()
+        {
+            var builder = new ContainerBuilder();
+            builder.Register<Mocks.IDecoratedType, Mocks.DecoratedType>(Lifetime.Singleton);
+            builder.RegisterDecorator<Mocks.IDecoratedType, Mocks.DecoratorWithAdditionalInterface>()
+                .As<Mocks.IOtherInterface>();
+
+            var container = builder.Build();
+            var instance = container.Resolve<Mocks.IDecoratedType>();
+            Assert.That(instance, Is.TypeOf<Mocks.DecoratorWithAdditionalInterface>());
+            
+            var otherInstance = container.Resolve<Mocks.IOtherInterface>();
+            Assert.That(otherInstance, Is.TypeOf<Mocks.DecoratorWithAdditionalInterface>());
+        }
+
+        [Test]
+        public void Decorate_Automatically_Binds_To_All_Bound_Base_Interfaces()
+        {
+            var builder = new ContainerBuilder();
+            builder.Register<Mocks.ExtendedDecoratedType>(Lifetime.Singleton)
+                .As<Mocks.IDecoratedType, Mocks.IExtendedDecoratedType>();
+            
+            builder.RegisterDecorator<Mocks.IExtendedDecoratedType, Mocks.ExtendedDecorator>();
+
+            var container = builder.Build();
+            var instance = container.Resolve<Mocks.IExtendedDecoratedType>();
+            Assert.That(instance, Is.TypeOf<Mocks.ExtendedDecorator>());
+            
+            var baseInstance = container.Resolve<Mocks.IDecoratedType>();
+            Assert.That(baseInstance, Is.TypeOf<Mocks.ExtendedDecorator>());
+        }
+
+
+        private static class Mocks
+        {
+            public interface IDecoratedType
+            {
+            }
+
+            public class DecoratedType : IDecoratedType
+            {
+            }
+
+            public class Decorator1 : IDecoratedType
+            {
+                public readonly IDecoratedType Inner;
+
+                public Decorator1(IDecoratedType inner)
+                {
+                    Inner = inner;
+                }
+            }
+
+            public class Decorator2 : IDecoratedType
+            {
+                public readonly IDecoratedType Inner;
+
+                public Decorator2(IDecoratedType inner)
+                {
+                    Inner = inner;
+                }
+            }
+            
+            public interface IOtherInterface
+            {
+            }
+            
+            public class DecoratedTypeWithNonRelatedInterface : IDecoratedType, IOtherInterface
+            {
+            }
+
+            public class DecoratorWithAdditionalInterface : IDecoratedType, IOtherInterface
+            {
+                public readonly IDecoratedType Inner;
+
+                public DecoratorWithAdditionalInterface(IDecoratedType inner)
+                {
+                    Inner = inner;
+                }
+            }
+            
+            public interface IExtendedDecoratedType : IDecoratedType
+            {
+            }
+
+            public class ExtendedDecoratedType : IExtendedDecoratedType
+            {
+            }
+
+            public class ExtendedDecorator : IExtendedDecoratedType
+            {
+                public readonly IExtendedDecoratedType Inner;
+
+                public ExtendedDecorator(IExtendedDecoratedType inner)
+                {
+                    Inner = inner;
+                }
+            }
         }
     }
 }
--- VContainer/Assets/VContainer/Runtime/DecoratorRegistrationBuilder.cs
+++ VContainer/Assets/VContainer/Runtime/DecoratorRegistrationBuilder.cs
@@ -1,55 +1,61 @@
 using System;
+using System.Collections.Generic;
 using VContainer.Internal;
 
 namespace VContainer.Runtime
 {
     public static class ConatinerBuilderDecoratorExtensions
     {
-        public static DecoratorRegistrationBuilder RegisterDecorator<TInterface, TDecorator>(this IContainerBuilder builder)
+        public static DecoratorRegistrationBuilder<TInner, TDecorator> RegisterDecorator<TInner, TDecorator>(this IContainerBuilder builder)
         {
             for (var i = builder.Count - 1; i >= 0; i--)
             {
                 var interfaceTypes = builder[i].InterfaceTypes;
-                if (interfaceTypes != null && interfaceTypes.Contains(typeof(TInterface)))
+                if (interfaceTypes != null && interfaceTypes.Contains(typeof(TInner)))
                 {
-                    var decoratorBuilder = new DecoratorRegistrationBuilder(builder[i], typeof(TDecorator));
+                    var decoratorBuilder = new DecoratorRegistrationBuilder<TInner, TDecorator>(builder[i]);
                     builder.Register(decoratorBuilder);
                     return decoratorBuilder;
                 }
             }
-            throw new VContainerException(typeof(TInterface), $"No such decorator target: {typeof(TInterface)}");
+            throw new VContainerException(typeof(TInner), $"No such decorator target: {typeof(TInner)}");
         }
 
-        public static DecoratorRegistrationBuilder RegisterDecorator<TInterface, TDecorator>(
+        public static FuncDecoratorRegistrationBuilder<TInner, TDecorator> RegisterDecorator<TInner, TDecorator>(
             this IContainerBuilder builder,
-            Func<TInterface, IObjectResolver, TDecorator> factory)
+            Func<TInner, IObjectResolver, TDecorator> factory)
         {
             for (var i = builder.Count - 1; i >= 0; i--)
             {
                 var interfaceTypes = builder[i].InterfaceTypes;
-                if (interfaceTypes != null && interfaceTypes.Contains(typeof(TInterface)))
+                if (interfaceTypes != null && interfaceTypes.Contains(typeof(TInner)))
                 {
-                    var decoratorBuilder = new FuncDecoratorRegistrationBuilder<TInterface, TDecorator>(builder[i], factory);
+                    var decoratorBuilder = new FuncDecoratorRegistrationBuilder<TInner, TDecorator>(builder[i], factory);
                     builder.Register(decoratorBuilder);
                     return decoratorBuilder;
                 }
             }
-            throw new VContainerException(typeof(TInterface), $"No such decorator target: {typeof(TInterface)}");
+            throw new VContainerException(typeof(TInner), $"No such decorator target: {typeof(TInner)}");
         }
     }
 
-    public class DecoratorRegistrationBuilder : RegistrationBuilder
+    public class DecoratorRegistrationBuilder<TInner, TDecorator> : RegistrationBuilder
     {
-        protected readonly RegistrationBuilder inner;
-        protected readonly Type interfaceType;
+        readonly RegistrationBuilder inner;
 
-        public DecoratorRegistrationBuilder(RegistrationBuilder inner, Type decoratorType)
-            : base(decoratorType, inner.Lifetime)
+        public DecoratorRegistrationBuilder(RegistrationBuilder inner)
+            : base(typeof(TDecorator), inner.Lifetime)
         {
             this.inner = inner;
-            interfaceType = inner.InterfaceTypes != null ? inner.InterfaceTypes[0] : inner.ImplementationType;
-            InterfaceTypes = inner.InterfaceTypes;
-            As(interfaceType);
+            
+            InterfaceTypes = new List<Type>();
+            for (int i = 0; i < inner.InterfaceTypes.Count; i++)
+            {
+                if (inner.InterfaceTypes[i].IsAssignableFrom(typeof(TInner)))
+                    InterfaceTypes.Add(inner.InterfaceTypes[i]);
+            }
+            
+            As<TInner>();
         }
 
         public override Registration Build()
@@ -62,21 +68,32 @@ namespace VContainer.Runtime
                 var innerInstance = container.Resolve(innerRegistration);
                 var parameters = new IInjectParameter[Parameters == null ? 1 : Parameters.Count];
                 Parameters?.CopyTo(parameters);
-                parameters[parameters.Length - 1] = new TypedParameter(interfaceType, innerInstance);
+                parameters[parameters.Length - 1] = new TypedParameter(typeof(TInner), innerInstance);
                 return injector.CreateInstance(container, parameters);
             });
             return new Registration(ImplementationType, Lifetime, InterfaceTypes, provider);
         }
     }
 
-    public class FuncDecoratorRegistrationBuilder<TInner, TDecorator> : DecoratorRegistrationBuilder
+    public class FuncDecoratorRegistrationBuilder<TInner, TDecorator> : RegistrationBuilder
     {
+        readonly RegistrationBuilder inner;
         readonly Func<TInner, IObjectResolver, TDecorator> factory;
 
         public FuncDecoratorRegistrationBuilder(RegistrationBuilder inner, Func<TInner, IObjectResolver, TDecorator> factory)
-            : base(inner, typeof(TDecorator))
+            : base(typeof(TDecorator), inner.Lifetime)
         {
+            this.inner = inner;
             this.factory = factory;
+            
+            InterfaceTypes = new List<Type>();
+            for (int i = 0; i < inner.InterfaceTypes.Count; i++)
+            {
+                if (inner.InterfaceTypes[i].IsAssignableFrom(typeof(TInner)))
+                    InterfaceTypes.Add(inner.InterfaceTypes[i]);
+            }
+            
+            As<TInner>();
         }
 
         public override Registration Build()
