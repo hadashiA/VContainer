d9f6dc6b68fc9ad36bce6b580f6405245c28f67d
Selakz <787276422@qq.com>
2026-02-25 18:28:26 +0800

    fix: pass registration key to resolve in RegisterComponent extensions

--- VContainer/Assets/VContainer/Runtime/Unity/ContainerBuilderUnityExtensions.cs
+++ VContainer/Assets/VContainer/Runtime/Unity/ContainerBuilderUnityExtensions.cs
@@ -132,7 +132,7 @@ namespace VContainer.Unity
         {
             var registrationBuilder = new ComponentRegistrationBuilder(component).As(typeof(TInterface));
             // Force inject execution
-            builder.RegisterBuildCallback(container => container.Resolve<TInterface>());
+            builder.RegisterBuildCallback(container => container.Resolve<TInterface>(registrationBuilder.Key));
             return builder.Register(registrationBuilder);
         }
 
@@ -151,7 +151,8 @@ namespace VContainer.Unity
                     container.Resolve(
                         registrationBuilder.InterfaceTypes != null
                             ? registrationBuilder.InterfaceTypes[0]
-                            : registrationBuilder.ImplementationType
+                            : registrationBuilder.ImplementationType,
+                        registrationBuilder.Key
                     );
                 }
             );
